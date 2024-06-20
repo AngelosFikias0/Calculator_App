404a1aa68204a6b9d31ce0cb4cc99d7a2be794eb
Angelos <angelosfikias@gmail.com>
2024-06-20 21:34:11 +0300

    Imrpovement of code

--- Calculator2_0.js
+++ Calculator2_0.js
@@ -4,6 +4,7 @@ const buttons = document.querySelectorAll('th');
 
 let expression = '';
 let result = '';
+let hasError = false; // Flag to track if an error has occurred
 
 const calc = document.getElementById('calc');
 const results = document.getElementById('results');
@@ -14,18 +15,29 @@ function buttonClick(event) {
     if (buttonValue === 'C') {
         expression = '';
         result = '';
+        if (hasError) {
+            results.style.backgroundColor = 'rgb(153, 137, 137)';
+            results.textContent = 'Please give correct input';
+            hasError = false; // Reset the error flag
+        } else {
+            results.textContent = 'Please input something';
+        }
     } else if (buttonValue === '←') {
-        expression = expression.slice(0,-1);
+        expression = expression.slice(0, -1);
     } else if (buttonValue === '=') {
         try {
-            if(expression === ''){
+            if (expression === '') {
                 results.textContent = 'Please input something';
-            }else{
-            result = eval(expression); 
-            results.textContent = result;}
+            } else {
+                result = eval(expression); 
+                results.textContent = result;
+                results.style.backgroundColor = 'rgb(153, 137, 137)';
+                hasError = false; 
+            }
         } catch (error) {
             results.textContent = 'Error';
             results.style.backgroundColor = 'Red';
+            hasError = true; 
         }
     } else {
         expression += buttonValue;
